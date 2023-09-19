e90cc0ccc85513eccc6a43b5be918f15af9c5228
RamazonDurdiyev <ramazon3684@gmail.com>
2023-09-19 13:07:13 +0500

    Base commit

---
+++ lib/ui/pages/home_page/home_page.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: _buildDrawer(),
+      body: _buildBody(context),
+    );
+  }
+
+  _buildDrawer() {
+    return Drawer(
+      backgroundColor: const Color.fromARGB(255, 41, 49, 106),
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          const UserAccountsDrawerHeader(
+            decoration: BoxDecoration(color: Colors.indigo),
+            accountName: Text(
+              'John Doe',
+              style: TextStyle(color: Colors.white),
+            ),
+            accountEmail: Text(
+              'johndoe@example.com',
+              style: TextStyle(color: Colors.white),
+            ),
+            currentAccountPicture: CircleAvatar(
+              backgroundColor: Colors.white,
+              child: Icon(
+                Icons.person_2,
+                size: 32,
+                color: Colors.indigo,
+              ),
+              // backgroundImage: AssetImage('assets/images/logo.png'),
+            ),
+          ),
+          ListTile(
+            leading: const Icon(
+              Icons.home_filled,
+              color: Colors.white,
+            ),
+            title: const Text(
+              'Home',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(
+              Icons.settings,
+              color: Colors.white,
+            ),
+            title: const Text(
+              'Settings',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            onTap: () {},
+          ),
+          const Divider(
+            color: Colors.grey,
+          ),
+          ListTile(
+            leading: const Icon(
+              Icons.exit_to_app,
+              color: Colors.white,
+            ),
+            title: const Text(
+              'Logout',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+
+  _buildBody(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: ScreenUtil().statusBarHeight + 8,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 8,
+          ),
+          child: Row(
+            children: [
+              Builder(builder: (context) {
+                return GestureDetector(
+                  onTap: () {
+                    Scaffold.of(context).openDrawer();
+                  },
+                  child: const Icon(
+                    Icons.analytics,
+                    size: 50,
+                    color: Colors.indigo,
+                  ),
+                );
+              }),
+              const SizedBox(
+                width: 8,
+              ),
+              const Text(
+                "Samo Techno",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+              const Expanded(
+                child: SizedBox(),
+              ),
+              const Icon(
+                Icons.notifications,
+                color: Colors.indigo,
+                size: 32,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/ui/pages/sign_up_page/sign_up_page.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:samo_crm/ui/pages/home_page/home_page.dart';
+import 'package:samo_crm/ui/pages/sign_in_page/sign_in_page.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _buildBody(context),
+    );
+  }
+
+  _buildBody(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Text(
+            "Create account",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        _buildTextField(
+          TextEditingController(),
+          "Phone number",
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        _buildTextField(
+          TextEditingController(),
+          "Password",
+        ),
+        const SizedBox(
+          height: 24,
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Text(
+            "Full name",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        _buildTextField(
+          TextEditingController(),
+          "Full name",
+        ),
+        const SizedBox(
+          height: 32,
+        ),
+        _buildSignButton(context),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Already have an account?",
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return const SignInPage();
+                    },
+                  ),
+                );
+              },
+              child: const Text(
+                "Sign In",
+                style: TextStyle(
+                  color: Colors.indigo,
+                ),
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  _buildSignButton(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return const HomePage();
+          },),);
+        },
+        style: ElevatedButton.styleFrom(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(16),
+            ),
+          ),
+          backgroundColor: Colors.indigo,
+          fixedSize: const Size(
+            double.maxFinite,
+            60,
+          ),
+        ),
+        child: const Text(
+          "Sign Up",
+        ),
+      ),
+    );
+  }
+
+  _buildTextField(TextEditingController ctrl, String hint) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: SizedBox(
+        child: TextField(
+          controller: ctrl,
+          cursorColor: const Color.fromARGB(255, 4, 27, 66),
+          style: const TextStyle(color: Colors.black),
+          decoration: InputDecoration(
+            suffixIcon: const Icon(
+              Icons.call,
+              color: Colors.grey,
+            ),
+            hintText: hint,
+            hintStyle: const TextStyle(
+                color: Colors.grey, fontSize: 16, fontFamily: "Lato"),
+            fillColor: const Color.fromARGB(255, 236, 234, 234),
+            filled: true,
+            border: const OutlineInputBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(16),
+                ),
+                borderSide: BorderSide.none),
+          ),
+        ),
+      ),
+    );
+  }
+}
